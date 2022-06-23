9485ecf9baf46ddee66c8359d272e1942977f8cc
LordDeveloper <losrkzpp@hi2.in>
2022-06-23 16:35:52 +0430

    Middleware was updated, "secure_token" was added

--- src/Middlewares/AuthorizeWebhooks.php
+++ src/Middlewares/AuthorizeWebhooks.php
@@ -12,14 +12,28 @@ class AuthorizeWebhooks implements Middleware
 {
 
     /**
+     * Authorize telegram webhook requests
+     *
      * @param Request $request
      * @param RequestHandler $next
      * @return Promise
      */
     public function handleRequest(Request $request, RequestHandler $next): Promise
     {
+        /**
+         * In the latest version of telegram the "secret_token" field was added when setting the webhook.
+         * This middleware can help you to authorize your with secret_token.
+         */
         return call(function () use ($request, $next) {
-            return $next->handleRequest($request, $next);
+            $env = strtolower(getenv('APP_ENV'));
+
+            if ($env === 'development') {
+                return $next->handleRequest($request, $next);
+            } elseif (getenv('APP_ENV') === 'production'
+                && $request->getHeader('X-Telegram-Bot-Api-Secret-Token') === getenv('SECURE_TOKEN'))
+                return $next->handleRequest($request, $next);
+
+            return false;
         });
     }
 }
