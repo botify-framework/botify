083d7bfa955cb3a4bcc34a8add0b69d5ea9131c7
LordDeveloper <losrkzpp@hi2.in>
2022-06-26 14:09:12 +0430

    Added support array access and formatted

--- src/Utils/LazyJsonMapper.php
+++ src/Utils/LazyJsonMapper.php
@@ -38,26 +38,48 @@ class LazyJsonMapper extends \LazyJsonMapper\LazyJsonMapper implements ArrayAcce
         return $this->asArray();
     }
 
+    /**
+     * Convert correct object to collection
+     *
+     * @return Collection
+     */
     public function collect(): Collection
     {
         return collect($this->toArray(), true);
     }
 
+    /**
+     * @param mixed $offset
+     * @return bool
+     */
     public function offsetExists(mixed $offset): bool
     {
         return $this->_isProperty($offset);
     }
 
+    /**
+     * @param mixed $offset
+     * @return mixed
+     */
     public function offsetGet(mixed $offset): mixed
     {
         return $this->_getProperty($offset);
     }
 
+    /**
+     * @param mixed $offset
+     * @param mixed $value
+     * @return void
+     */
     public function offsetSet(mixed $offset, mixed $value): void
     {
         $this->_setProperty($offset, $value);
     }
 
+    /**
+     * @param mixed $offset
+     * @return void
+     */
     public function offsetUnset(mixed $offset): void
     {
         $this->_unsetProperty($offset);
