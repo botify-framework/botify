6c132954ebf25ee1d44166425a4b0e69a9427cf9
LordDeveloper <losrkzpp@hi2.in>
2022-06-28 21:31:31 +0430

    Bug?

--- config/telegram.php
+++ config/telegram.php
@@ -3,8 +3,12 @@
 return [
     'token' => env('BOT_TOKEN', ''),
     'super_admin' => (int)env('SUPER_ADMIN'),
-    'admins' => array_merge([config('super_admin')], array_filter(array_map(
-        fn($admin) => (int)trim($admin), explode(',', env('ADMINS'),
-    )))),
+    'admins' => function () {
+        $admins = array_filter(array_map(
+            fn($admin) => (int)trim($admin), explode(',', env('ADMINS'),
+        )));
+
+        return array_unshift($admins, config('telegram.super_admin'));
+    },
     'cache_chat' => env('CACHE_CHAT'),
 ];
