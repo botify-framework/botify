29ec5199743c47c0e5fe090336ae1397dc39cee4
LordDeveloper <losrkzpp@hi2.in>
2022-07-04 14:17:57 +0430

    Added startsWith and endsWith method

--- src/Traits/Stringable.php
+++ src/Traits/Stringable.php
@@ -5,6 +5,20 @@ namespace Jove\Traits;
 trait Stringable
 {
 
+    public function endsWith($needles): bool
+    {
+        return array_some(
+            (array)$needles, fn($needle) => str_ends_with($this->getStringableValue(), $needle)
+        );
+    }
+
+    public function startsWith($needles): bool
+    {
+        return array_some(
+            (array)$needles, fn($needle) => str_starts_with($this->getStringableValue(), $needle)
+        );
+    }
+
     public function is($value): bool
     {
         return in_array($this->getStringableValue(), (array)$value);
