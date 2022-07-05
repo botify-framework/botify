c653e1fbe7909801c0fc6fc7175b68de943b15ef
LordDeveloper <losrkzpp@hi2.in>
2022-07-06 02:32:17 +0430

    [updated] improved Stringable and added after, afterLast, before, beforeLast

--- src/Traits/Stringable.php
+++ src/Traits/Stringable.php
@@ -4,28 +4,126 @@ namespace Jove\Traits;
 
 trait Stringable
 {
+    /**
+     * Get string after a phrase
+     *
+     * @param string $search
+     * @return string
+     */
+    public function after(string $search): string
+    {
+        return $search === '' ? $this->value() : value(function () use ($search) {
+            $split = explode($this->value(), $search, 2);
+
+            return end($split);
+        });
+    }
+
+    /**
+     * Get string after latest a phrase
+     *
+     * @param string $search
+     * @return string
+     */
+    public function afterLast(string $search): string
+    {
+        return $search === '' ? $this->value() : value(function () use ($search) {
+            if (false === $position = mb_strrpos($this->value(), $search)) {
+                return $this->value();
+            }
+
+            return mb_substr($this->value(), $position + mb_strlen($search));
+        });
+    }
+
+    /**
+     * Get string before a phrase
+     *
+     * @param string $search
+     * @return string
+     */
+    public function before(string $search): string
+    {
+        return $search === '' ? $this->value() : value(function () use ($search) {
+            $result = strstr($this->value(), $search, true);
+
+            return $result === false ? $this->value() : $result;
+        });
+    }
+
+    /**
+     * Get string before latest a phrase
+     *
+     * @param string $search
+     * @return string
+     */
+    public function beforeLast(string $search): string
+    {
+        return $search === '' ? $this->value() : value(function () use ($search) {
+            if (false === $position = mb_strpos($this->value(), $search)) {
+                return $this->value();
+            }
+
+            return mb_substr($this->value(), 0, $position);
+        });
+    }
+
+    /**
+     * Alias of getStringableValue method
+     *
+     * @return ?string
+     */
+    private function value(): ?string
+    {
+        return $this->getStringableValue();
+    }
+
+    /**
+     * Check string ends with a needle
+     *
+     * @param $needles
+     * @return bool
+     */
     public function endsWith($needles): bool
     {
         return array_some(
-            (array)$needles, fn($needle) => str_ends_with($this->getStringableValue(), $needle)
+            (array)$needles, fn($needle) => str_ends_with($this->value(), $needle)
         );
     }
 
+    /**
+     * Check string equals to another
+     *
+     * @param $value
+     * @return bool
+     */
+    public function eq($value): bool
+    {
+        return in_array($this->value(), (array)$value);
+    }
+
+    /**
+     * Check string starts with a needle
+     *
+     * @param $needles
+     * @return bool
+     */
     public function startsWith($needles): bool
     {
         return array_some(
-            (array)$needles, fn($needle) => str_starts_with($this->getStringableValue(), $needle)
+            (array)$needles, fn($needle) => str_starts_with($this->value(), $needle)
         );
     }
 
-    public function is($value): bool
+    /**
+     * Trim string from whitespaces or specified characters
+     *
+     * @param string $characters
+     * @return string
+     */
+    public function trim(string $characters = " \t\n\r\0\x0B"): string
     {
-        return in_array($this->getStringableValue(), (array)$value);
-    }
-
-    public function trim($characters = " \t\n\r\0\x0B")
-    {
-        return trim($this->getStringableValue(), $characters);
+        return trim($this->value(), $characters);
     }
 
     abstract protected function getStringableValue(): ?string;
