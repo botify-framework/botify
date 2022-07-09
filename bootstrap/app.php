13c1db01b6bae46f23faa09ce4445c870a75f0b4
LordDeveloper <losrkzpp@hi2.in>
2022-07-10 00:42:02 +0430

    [updated] Synced with phar version

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,10 @@
 <?php
 
+defined('__BASE_DIR__') || define('__BASE_DIR__', $_ENV['__BASE_DIR__'] ?? __DIR__ . '/../');
+
 define('START_TIME', microtime(true));
 
 require_once __DIR__ . '/../vendor/autoload.php';
-
 
 $repository = Dotenv\Repository\RepositoryBuilder::createWithNoAdapters()
     ->addAdapter(Dotenv\Repository\Adapter\EnvConstAdapter::class)
@@ -11,7 +12,7 @@ $repository = Dotenv\Repository\RepositoryBuilder::createWithNoAdapters()
     ->immutable()
     ->make();
 
-$dotenv = Dotenv\Dotenv::create($repository, __DIR__ . '/..');
+$dotenv = Dotenv\Dotenv::create($repository, __BASE_DIR__, ['.env', '.env.example']);
 $dotenv->load();
 
 date_default_timezone_set(config('app.timezone'));
