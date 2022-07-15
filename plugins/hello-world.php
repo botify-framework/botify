71c222bcd74559609095cb5e69069e94281047ba
LordDeveloper <losrkzpp@hi2.in>
2022-07-15 21:20:42 +0430

    [fixed] edited_message bug fixed

--- plugins/hello-world.php
+++ plugins/hello-world.php
@@ -1,15 +1,20 @@
 <?php
 
 
+use Jove\TelegramAPI;
 use Jove\Types\Map\Message;
+use Jove\Types\Update;
 use Jove\Utils\Plugins\Pluggable;
 
-return new class() extends Pluggable {
+$filters = [
+    fn(TelegramAPI $api, Update $update) => isset($update['message']) || isset($update['edited_message'])
+];
+return new class($filters) extends Pluggable {
     public Message $message;
 
     public function boot()
     {
-        $this->message = $this->update['message'];
+        $this->message = $this->update['message'] ?? $this->update['edited_message'];
     }
 
     public function handle()
