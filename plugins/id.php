6b763ec1b1578457a8c8dedab71591ff9a4ded83
LordDeveloper <losrkzpp@hi2.in>
2022-07-15 21:11:08 +0430

    [updated] Added id plugin

---
+++ plugins/id.php
@@ -0,0 +1,22 @@
+<?php
+
+
+use Jove\TelegramAPI;
+use Jove\Types\Update;
+use Jove\Utils\Plugins\Plugin;
+
+$filters = [
+    fn(TelegramAPI $api, Update $update) => isset($update['message'])
+];
+
+return Plugin::apply(function () {
+    $message = $this->message;
+
+    if ($message->eq('/id')) {
+        if (isset($message['reply_to_message'])) {
+            yield $message->reply($message['reply_to_message']['from']['id']);
+        } else {
+            yield $message->reply($message['from']['id']);
+        }
+    }
+});
