fbc7ae9c18664619d1bfc4760df6cd6e67055dd7
LordDeveloper <losrkzpp@hi2.in>
2022-07-26 15:18:38 +0430

    [updated] id plugin was updated

--- plugins/id.php
+++ plugins/id.php
@@ -6,7 +6,32 @@ use Jove\Utils\Plugins\Plugin;
 return Plugin::apply(function (Message $message) {
     if ($message->command(['id', 'info', 'me'])) {
         $message = $message['reply_to_message'] ?? $message;
+        $from = $message['from'];
 
-        yield $message->reply($message['from']['id']);
+        $photos = yield $from->getProfilePhotos(limit: 10);
+        $caption = sprintln('First name: ' . $from['first_name']);
+        $caption .= isset($from['last_name']) ? sprintln('Last name: ' . $from['last_name']) : '';
+        $caption .= sprintln('ID: ' . $from['id']);
+
+        if (in_array($message['chat']['type'], ['supergroup', 'group'])) {
+            $chat = $message['chat'];
+            $caption .= sprintln('Group Information');
+            $caption .= sprintln('Title: ' . $chat['title']);
+            $caption .= sprintln('ID: ' . $chat['id']);
+        }
+
+        if ($photos->isNotEmpty()) {
+            $media = $photos->map(function ($photo) {
+                return [
+                    'type' => 'photo',
+                    'media' => end($photo)['file_id'],
+                ];
+            })->toArray();
+            $media[array_key_last($media)]['caption'] = $caption;
+
+            return yield $message->replyMediaGroup($media, caption: $caption);
+        }
+
+        yield $message->reply($caption);
     }
 });
