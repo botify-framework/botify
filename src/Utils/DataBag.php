f633d1913fbb976554197645c66d85c7a9163cb4
LordDeveloper <losrkzpp@hi2.in>
2022-07-30 01:30:39 +0430

    [updated] Added DataBag for access a data in all plugin and update handlers

---
+++ src/Utils/DataBag.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Botify\Utils;
+
+use ArrayAccess;
+use Botify\TelegramAPI;
+
+class DataBag implements ArrayAccess
+{
+    protected array $attributes = [];
+
+    public function __construct(public TelegramAPI $api)
+    {
+    }
+
+    public function __call($method, array $arguments = [])
+    {
+        return $this->api->{$method}(... $arguments);
+    }
+
+    public function __get(string $name)
+    {
+        return $this->attributes[$name] ?? null;
+    }
+
+    public function __set(string $name, mixed $value)
+    {
+        $this->attributes[$name] = $value;
+    }
+
+    public function offsetExists(mixed $offset): bool
+    {
+        return isset($this->attributes[$offset]);
+    }
+
+    public function offsetGet(mixed $offset): mixed
+    {
+        return $this->attributes[$offset] ?? null;
+    }
+
+    public function offsetSet(mixed $offset, mixed $value): void
+    {
+        if (!is_null($offset)) {
+            $this->attributes[$offset] = $value;
+        }
+    }
+
+    public function offsetUnset(mixed $offset): void
+    {
+        unset($this->attributes[$offset]);
+    }
+}
