4e180e46550f571049084ce83b42e89474d46c5a
lorddeveloper <losrkzpp@hi2.in>
2022-09-06 18:06:51 +0430

    Renamed readHistory to getHistory, Fixed return type problem

--- bot.php
+++ bot.php
@@ -13,10 +13,20 @@ $bot = TelegramAPI::factory();
 $bot->setEventHandler(new class extends EventHandler {
     public function onUpdateNewMessage(Message $message)
     {
+        if (isset($message['reply_to_message'])) {
+            $replies = $message['reply_to_message']->getReplies();
+            while (yield $replies->advance()) {
+                var_dump($replies->getCurrent()->text);
+            }
+        }
+
         yield $message->reply("Hi {$message['from']['first_name']} :)");
     }
 });
 
+$bot->on('mention', function (Message $message) {
+    yield $message->reply('جانم');
+}).
 // You can use different update handing types like UPDATE_TYPE_WEBHOOK,
 // UPDATE_TYPE_HTTP_SERVER and UPDATE_TYPE_LONG_POLLING
 // Note: default type is UPDATE_TYPE_WEBHOOK.
